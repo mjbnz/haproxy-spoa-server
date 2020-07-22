55c37a5a969f26e921fb8f69d95b3a09c5ab559c
Mike Beattie - Internet by Design Limited <mike@ethernal.org>
2020-07-22 00:23:18 +0000

    Add ability to enable debug mode
    Signed-off-by: Mike Beattie - Internet by Design Limited <mike@ethernal.org>

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,7 @@ if [ ! -f /data/spoa_python.py ];then
     cp /ps_python.py /data/spoa_python.py
 fi
 
-exec /spoa_server -f /data/spoa_python.py
+[ -n "$DEBUG" ] && DEBUG="-d"
+
+exec /spoa_server ${DEBUG} -f /data/spoa_python.py
 
