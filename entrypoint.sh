fda41971573d280854199f3affefd74cdfc35981
Mike Beattie - Internet by Design Limited <mike@ethernal.org>
2020-08-06 11:16:20 +0000

    Change DEBUG variable to a generic ARGS variable
    Signed-off-by: Mike Beattie <mike@ethernal.org>

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,5 @@ if [ ! -f /data/spoa_python.py ];then
     cp /ps_python.py /data/spoa_python.py
 fi
 
-[ -n "$DEBUG" ] && DEBUG="-d"
-
-exec /spoa_server ${DEBUG} -f /data/spoa_python.py
+exec /spoa_server ${ARGS} -f /data/spoa_python.py
 
